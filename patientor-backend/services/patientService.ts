6449f08236ff4c00d0d82f2def46c1558a6f917f
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-05-20 18:35:35 +0300

    9.11: Patientor backend, step4

---
+++ patientor-backend/services/patientService.ts
@@ -0,0 +1,23 @@
+import patientData from '../data/patients.json';
+import { PatientEntry, NonSensitivePatientEntry } from '../types';
+
+const patients: Array<PatientEntry> = patientData;
+
+const getEntries = (): Array<PatientEntry> => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getEntries,
+  getNonSensitiveEntries,
+};
