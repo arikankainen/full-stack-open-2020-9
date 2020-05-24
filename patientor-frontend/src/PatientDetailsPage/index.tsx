1e2628a19a12c204c64cbd694dc75511b8262f96
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-05-24 20:16:40 +0300

    9.24: patientor, step9

--- patientor-frontend/src/PatientDetailsPage/index.tsx
+++ patientor-frontend/src/PatientDetailsPage/index.tsx
@@ -4,15 +4,42 @@ import { useStateValue, updatePatient } from "../state";
 import { Patient } from '../types';
 import axios from 'axios';
 import { apiBaseUrl } from "../constants";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Button } from "semantic-ui-react";
 import EntryDetails from './EntryDetails';
 import GenderIcon from './GenderIcon';
+import AddEntryModal from '../AddEntryModal';
+import { EntryFormValues } from '../AddEntryModal/AddEntryForm';
 
 const PatientDetailsPage: React.FC = () => {
   const [{ patients }, dispatch] = useStateValue();
   
   const { id } = useParams<{ id: string }>();
   const patient: Patient = patients[id];
+
+  const [modalOpen, setModalOpen] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>();
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
+  };
+
+  const submitNewPatient = async (values: EntryFormValues) => {
+    try {
+      const { data: updatedPatient } = await axios.post<Patient>(
+        `${apiBaseUrl}/patients/${id}/entries`,
+        values
+      );
+      if (!updatePatient) throw new Error(`Patient with id ${id} not found`);
+      dispatch(updatePatient(updatedPatient));
+      closeModal();
+    } catch (e) {
+      console.error(e.response.data);
+      setError(e.response.data.error);
+    }
+  };
 
   useEffect(() => {
     const checkPatientData = async () => {
@@ -46,6 +73,15 @@ const PatientDetailsPage: React.FC = () => {
       <div>occupation: {patient.occupation}</div>
       
       <Header as='h3'>entries</Header>
+      
+      <AddEntryModal
+        modalOpen={modalOpen}
+        onSubmit={submitNewPatient}
+        error={error}
+        onClose={closeModal}
+      />
+      <Button onClick={() => openModal()}>Add New Entry</Button>
+
       {patient.entries?.map(entry =>
         <EntryDetails key={entry.id} entry={entry}/>
       )}
--- patientor-frontend/src/state/reducer.ts
+++ patientor-frontend/src/state/reducer.ts
@@ -14,10 +14,10 @@ export type Action =
     type: "UPDATE_PATIENT";
     payload: Patient;
     }
-    | {
-      type: "SET_DIAGNOSIS_LIST";
-      payload: Diagnosis[];
-    };
+  | {
+    type: "SET_DIAGNOSIS_LIST";
+    payload: Diagnosis[];
+  };
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
@@ -40,26 +40,26 @@ export const reducer = (state: State, action: Action): State => {
           [action.payload.id]: action.payload
         }
       };
-      case "UPDATE_PATIENT":
-        return {
-          ...state,
-          patients: {
-            ...state.patients,
-            [action.payload.id]: action.payload
-          }
-        };
-        case "SET_DIAGNOSIS_LIST":
-          return {
-            ...state,
-            diagnosis: {
-              ...action.payload.reduce(
-                (memo, diagnosis) => ({ ...memo, [diagnosis.code]: diagnosis }),
-                {}
-              ),
-              ...state.diagnosis
-            }
-          };
-          default:
+    case "UPDATE_PATIENT":
+      return {
+        ...state,
+        patients: {
+          ...state.patients,
+          [action.payload.id]: action.payload
+        }
+      };
+    case "SET_DIAGNOSIS_LIST":
+      return {
+        ...state,
+        diagnosis: {
+          ...action.payload.reduce(
+            (memo, diagnosis) => ({ ...memo, [diagnosis.code]: diagnosis }),
+            {}
+          ),
+          ...state.diagnosis
+        }
+      };
+    default:
       return state;
   }
 };
